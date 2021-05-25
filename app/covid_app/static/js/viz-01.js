a9af71e6c667d77d30bd21e51bcc1d94805ced5e
the-other-mariana <mariana.avalos.arce@gmail.com>
2021-05-25 17:28:28 -0500

    tabaquism viz animated
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- app/covid_app/static/js/viz-01.js
+++ app/covid_app/static/js/viz-01.js
@@ -2,8 +2,8 @@ console.log(data_men)
 console.log(data_women)
 
 var margin = {top: 50, right: 10, bottom: 100, left:100};
-var width = 800;
-var height = 600;
+var width = 600;
+var height = 400;
 
 var svg = d3.select("#chart-area")
 	.append("svg")
@@ -13,79 +13,126 @@ var svg = d3.select("#chart-area")
 var g = svg.append("g")
     .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
-data = data_men
-
-data.forEach((d)=>{
-    d.revenue = parseInt(d.count);
-});
-
-var tab = data.map((d) => { return d.tabaquismo; });
+var data = data_men
 
 var x = d3.scaleBand()
-        .domain(tab)
-        .range([0, width])
-        .paddingInner(0.2)
-        .paddingOuter(0.3);
-
-var maxCount = d3.max(data, (d) => { return d.count; });
+            .range([0, width])
+            .paddingInner(0.2)
+            .paddingOuter(0.3);
 
 var y = d3.scaleLinear()
-        .domain([maxCount, 0])
         .range([0, height]);
 
-var rects = g.selectAll("rect").data(data);
-rects.enter()
-    .append("rect")
-    .attr("x", (d) => {
-        return x(d.tabaquismo);
-    })
-    .attr("y", (d) => {
-        return y(d.count);
-    })
-    .attr("height", (d) => {
-        return height - y(d.count);
-    })
-    .attr("width", x.bandwidth())
-    .attr("fill", (d) => {
-        return "blue";
-    });
+var color = d3.scaleOrdinal()
+        .range(d3.schemeSet2);
 
-// bottom axis ticks
-var bottomAxis = d3.axisBottom(x);
-    g.append("g")
+var xAxisGroup = g.append("g")
     .attr("class", "bottom axis")
     .attr("transform", "translate(0, " + height + ")")
-    .call(bottomAxis)
-    .selectAll("text")
-    .attr("y", "10")
-    .attr("x", "-5")
-    .attr("filled", "black")
+
+var yAxisGroup = g.append("g")
+    .attr("class", "y axis")
+
+var yLabel = g.append("text")
+    .attr("class", "y axis-label")
+    .attr("x", - (height / 2))
+    .attr("y", -60)
+    .style("font", "18px century")
     .attr("text-anchor", "middle")
+    .attr("transform", "rotate(-90)")
+    .text("Revenue (dlls.)");
 
-// left y axis
-var yAxisCall = d3.axisLeft(y)
-    .ticks(10)
-    .tickFormat((d) => { return d; });
+var tab;
+var maxCount;
+var flag = true;
+var t = d3.transition().duration(750);
 
-g.append("g")
-    .call(yAxisCall);
+function update(data){
+    data = flag ? data_women : data_men;
 
-// x axis label
-g.append("text")
-.attr("class", "x axis-label")
-.attr("x", (width / 2))
-.attr("y", height + 140)
-.attr("font-size", "30px")
-.attr("text-anchor", "middle")
-.attr("transform", "translate(0, -70)")
-.text("Does the person smoke?");
+    tab = data.map((d) => { return d.tabaquismo; });
+    maxCount = d3.max(data, (d) => { return d.count; });
 
-// y axis label
-g.append("text")
-.attr("class", "y axis-label")
-.attr("x", - (height / 2))
-.attr("y", -60)
-.attr("font-size", "30px")
-.attr("text-anchor", "middle")
-.attr("transform", "rotate(-90)")
-.text("Number of People");
+    x.domain(tab)
+    y.domain([maxCount, 0])
+    color.domain(tab)
+
+    var bottomAxis = d3.axisBottom(x);
+    xAxisGroup.transition(t).call(bottomAxis)
+        .selectAll("text")
+        .attr("y", "10")
+        .attr("x", "-5")
+        .attr("filled", "black")
+        .attr("text-anchor", "middle")
+
+    var yAxisCall = d3.axisLeft(y)
+        .ticks(10)
+	    .tickFormat((d) => { return d; });
+
+	yAxisGroup.transition(t).call(yAxisCall);
+
+	// x axis label
+    g.append("text")
+    .attr("class", "x axis-label")
+    .attr("x", (width / 2))
+    .attr("y", height + 140)
+    .style("font", "18px century")
+    .attr("text-anchor", "middle")
+    .attr("transform", "translate(0, -70)")
+    .text("Does the person smoke?");
+
+    var label = flag ? "Women" : "Men";
+    yLabel.text(label)
+
+    var rects = g.selectAll("rect").data(data, (d) => { return d.tabaquismo; });
+    rects.exit().transition(t)
+		.attr("y", y(0))
+		.attr("height", 0).remove();
+
+    rects.transition(t)
+        .attr("x", (d) => { return x(d.tabaquismo); })
+	    .attr("y", (d) => { return y(d.count); })
+	    .attr("width", x.bandwidth)
+	    .attr("height",(d) => { return height - y(d.count)});
+
+    var rects = g.selectAll("rect").data(data);
+    rects.enter()
+        .append("rect")
+        .attr("x", (d) => {
+            return x(d.tabaquismo);
+        })
+        .attr("y", (d) => {
+            return y(d.count);
+        })
+        .attr("height", (d) => {
+            return height - y(d.count);
+        })
+        .attr("width", x.bandwidth())
+        .attr("fill", (d) => {
+            return color(d.tabaquismo);
+        })
+        .merge(rects)
+        .transition(t)
+                .attr("x", (d) => { return x(d.tabaquismo) })
+                .attr("width", x.bandwidth)
+                .attr("y", (d) => { return y(d.count); })
+                .attr("height", (d) => { return height - y(d.count); })
+                .attr("fill", (d) => {
+                    return color(d.tabaquismo);
+                });
+}
+
+function viz(){
+    data.forEach((d)=>{
+        d.revenue = parseInt(d.count);
+    });
+    d3.interval( ( ) => {
+		update(data);
+        console.log("Updating...");
+        flag = !flag;
+	}, 1000);
+    update(data);
+}
+
+viz();
+
